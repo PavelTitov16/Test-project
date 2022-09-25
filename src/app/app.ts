f0b70e51afaa423860298909c6fa7eea2fd169c8
PavelTitov16 <pashoktitov16@gmail.com>
2022-09-26 00:29:14 +0300

    feat: add circles module generation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,23 @@
 import { ApppModel } from '../models/app.model';
 import { Laptop } from '../modules/laptop';
 import { Description } from '../modules/description';
+import { Circle } from '../modules/circle';
 
 export class App implements ApppModel {
     public template: string;
     public body = document.querySelector('body') as HTMLBodyElement;
     public laptop: Laptop;
     public description: Description;
+    public circle: Circle;
 
-  constructor() {
-    this.laptop = new Laptop();
-    this.description = new Description();
-  }
-  
+    constructor() {
+        this.laptop = new Laptop();
+        this.description = new Description();
+        this.circle = new Circle();
+    }
+
     public init(): string {
-      this.template = `
+        this.template = `
         <div class="layout" id="layout">
             <div class="layout-container">
                 <h1 class="layout-title">Кейсы</h1>
@@ -22,7 +25,15 @@ export class App implements ApppModel {
                 ${this.description.init()}
             </div>
         </div>`;
-      return this.template;
+        return this.template;
+    }
+
+    public generateCircles(): void {
+        window.addEventListener('load', () => {
+            for (let i =1; i <= 3; i++) {
+                this.circle.init();
+            }
+        });
     }
 
     public async render(): Promise<void> {
@@ -31,5 +42,6 @@ export class App implements ApppModel {
         this.body.classList.add('color-green');
         this.description.subscribeOnLeft();
         this.description.subscribeOnRight();
+        this.generateCircles();
     }
 }
---
+++ src/modules/circle.ts
@@ -0,0 +1,30 @@
+import { CircleModel } from "../models/circle.model";
+import { getRandom } from "./functions";
+
+export class Circle implements CircleModel {
+    public size: number;
+
+    constructor() {
+        this.size = getRandom(180, 330);
+    }
+
+    public init(): void {
+        const layout = document.getElementById('layout') as HTMLDivElement;
+        const circle = document.createElement('div');
+        const { width, height } = layout.getBoundingClientRect();
+        const h = getRandom(0, height - this.size);
+        const w = getRandom(0, width - this.size);
+        const leftBtn = document.getElementById('btn-left') as HTMLButtonElement;
+
+        circle.classList.add('layout-circle');
+        circle.style.height = `${this.size}px`;
+        circle.style.width = `${this.size}px`;
+        circle.style.top = `${h}px`;
+        circle.style.left = `${w}px`;
+
+        if (leftBtn.disabled === true) {
+            circle.classList.add('green-circle');
+        } else circle.classList.add('purple-circle');
+        layout.append(circle);
+    }
+}
